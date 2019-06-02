96eade5c4d1e84da2ff833889ba331b77eb15db5
ecthyr <arreisnahtan@gmail.com>
2019-06-02 18:09:26 -0400

    Updates

---
+++ precache-manifest.4135e1a3d520309524d92d919bdb61ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aae370a6e720ff597eba7bc92ea6deb",
+    "url": "/dice-rollah/index.html"
+  },
+  {
+    "revision": "310afefd26d5bc607ce3",
+    "url": "/dice-rollah/static/css/main.9f4df4f3.chunk.css"
+  },
+  {
+    "revision": "5c97975fb6686de51be0",
+    "url": "/dice-rollah/static/js/2.f79c89c8.chunk.js"
+  },
+  {
+    "revision": "310afefd26d5bc607ce3",
+    "url": "/dice-rollah/static/js/main.c6e5826c.chunk.js"
+  },
+  {
+    "revision": "db62be7c4f2a5b9b8885",
+    "url": "/dice-rollah/static/js/runtime~main.d78cd5f5.js"
+  }
+]);
